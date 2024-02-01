62f7c7209a88cbb84f095b22f3bf87717d7dc13d
unknown <hercan2024>
2024-02-01 13:23:33 +0300

    v2

--- src/AtmProject.java
+++ src/AtmProject.java
@@ -2,6 +2,8 @@ public class AtmProject {
     public static void main(String[] args) {
         System.out.println("demo1");
         //version1
+        System.out.println("demo2");
+        //version2
     }
 
 }
